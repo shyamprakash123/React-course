8906f165856a8eb46f812d1711f1ec1da3dae534
Shyam prakash <shyamprakash9959@gmail.com>
2023-08-10 19:26:10 +0530

    Resolved errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import AppContainer from './AppContainer';
+import Header from './Header';
 
 function App() {
+  const formFields = [
+    {
+      title: "First Name",
+      type: "text"
+    },
+    {
+      title: "Last Name",
+      type: "text"
+    },
+    {
+      title: "Email",
+      type: "email"
+    },
+    {
+      title: "Date of Birth",
+      type: "date"
+    },
+    {
+      title: "Phone Number",
+      type: "tel"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
+        <Header title="Welcome to Lesson 5 of #react-typescript with #tailwindcss" />
+        {
+          formFields.map((field, id) => (
+            <div key={id}>
+              <label className='font-semibold'>{field.title}</label>
+              <input className='border-2 border-gray-200 focus:border-sky-500 focus:outline-none rounded-lg p-2 m-2 w-full' type={field.type} required />
+            </div>
+          ))
+        }
+        <div className='flex justify-center'>
+          <button className='p-2 m-2 w-20 bg-blue-500 rounded-xl hover:bg-blue-600 text-white font-bold text-base'>Submit</button>
+        </div>
+      </div>
+
+    </AppContainer>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+export default function Header(props: { title: string }) {
+    return (
+        <div className="flex gap-2 items-center">
+            <img src={logo} className="animate-spin h-16 w-16" alt="logo" />
+            <h1 className="text-center text-xl flex-1 font-semibold">{props.title}</h1>
+        </div>
+    )
+}
