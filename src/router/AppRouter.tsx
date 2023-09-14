fe9c08d455a86afc02a21f3899376c399946c9bf
Shyam prakash <shyamprakash9959@gmail.com>
2023-09-14 22:24:18 +0530

    Added suspense

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,21 @@
 import { useRoutes } from "raviger";
 import About from "../components/About";
 import AppContainer from "../AppContainer";
-import Home from "../components/Home";
 import Form from "../components/Form";
 import PreviewPage from "../components/PreviewPage";
 import QuizNotFound from "../components/QuizNotFound";
 import Login from "../components/Login";
 import Unauthenticated from "../components/Unauthenticated";
 import { User } from "../types/userTypes";
+import React from "react";
+const Home = React.lazy(() => import("../components/Home"));
 
 const routes = {
-  "/": () => <Home />,
+  "/": () => (
+    <React.Suspense fallback={<h1>Loading...</h1>}>
+      <Home />
+    </React.Suspense>
+  ),
   "/about": () => <About />,
   "/login": () => <Login />,
   "/forms/:id": ({ id }: { id: string }) => <Form id={Number(id)} />,
